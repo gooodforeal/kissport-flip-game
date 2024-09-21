81a29e86de7f80a7500fae3fbdceb808179d2fba
gooodforeal <2390603@gmail.com>
2024-09-21 20:20:40 +0300

    added tests

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 
 
 def process(command):
@@ -12,17 +11,18 @@ def process(command):
     )
 
 
-def expect(process, pattern):
-    buffer = ''
+def expect(proc, pattern):
+    pattern = pattern.strip("\n")
+    buffer = ""
     while True:
-        buffer += process.stdout.read(1).decode()
-        if buffer.endswith(pattern):
-            return buffer
+        buffer += proc.stdout.read(1).decode()
+        if pattern.endswith(buffer):
+            return True
 
 
-def write(process, text):
-    process.stdin.write(f'{text}\n'.encode())
-    process.stdin.flush()
+def write(proc, text):
+    proc.stdin.write(f'{text}\n'.encode())
+    proc.stdin.flush()
     return text
 
 
@@ -33,7 +33,7 @@ def test():
         py = process('python gamenornd.py')
 
         # Проверка, что программы печатают в консоль (stdout) одно и то же:
-        expected_greedings = '''
+        expected_greetings = '''
                          FLIP
                   CREATIVE COMPUTING
                 MORRISTOWN NEW JERSEY
@@ -43,12 +43,14 @@ def test():
 EXPLANATION (Y OR N)? 
 '''
         print("expecting answers...")
-        expect(bas, expected_greedings)
-        expect(py, expected_greedings)
+        expect(bas, expected_greetings)
+        expect(py, expected_greetings)
+        print("[+] TEST 1 - PASSED")
         # Печать ответа пользователя в stdin:
         print("sending keys...")
         write(bas, 'Y')
         write(py, 'Y')
+        print("[+] KEYS SENT")
         instruction = '''
 ON EACH TURN, YOU GUESS YES <'Y'> OR NO <'N'>.
 ONLY ONE IS CORRECT, AND THE PROGRAM HAS DECIDED
@@ -65,11 +67,66 @@ CHARACTER OF THE FOLLOWING LINE.
 
 
 BEGIN.
- Y 
+ ? 
 '''
         print("expecting results")
         expect(bas, instruction)
         expect(py, instruction)
+        print("[+] TEST 2 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'Y')
+        write(py, 'Y')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, "*? ")
+        expect(py, "*? ")
+        print("[+] TEST 3 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'Y')
+        write(py, 'Y')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, "*? ")
+        expect(py, "*? ")
+        print("[+] TEST 4 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'Y')
+        write(py, 'Y')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, "*? ")
+        expect(py, "*? ")
+        print("[+] TEST 5 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'N')
+        write(py, 'N')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, " ? ")
+        expect(py, " ? ")
+        print("[+] TEST 6 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'N')
+        write(py, 'N')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, " ? ")
+        expect(py, " ? ")
+        print("[+] TEST 7 - PASSED")
+
+        print("sending keys...")
+        write(bas, 'b')
+        write(py, 'b')
+        print("[+] KEYS SENT")
+        print("expecting answers...")
+        expect(bas, "ERROR, MUST BE Y OR N .\n? ")
+        expect(py, "ERROR, MUST BE Y OR N .\n? ")
+        print("[+] TEST 8 - PASSED")
     except Exception as ex:
         print(ex)
 
